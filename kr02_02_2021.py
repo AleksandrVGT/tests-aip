9d6e7a966fb28d53f2c4e80a9a8fb04a276c9034
AleksandrVGT <78807582+AleksandrVGT@users.noreply.github.com>
2021-02-09 19:23:17 +0300

    Update kr02_02_2021.py

--- kr02_02_2021.py
+++ kr02_02_2021.py
@@ -5,6 +5,14 @@ def task_1(two_dim_words):
         Заполнять список значениями не нужно.
         Финальное значение должно быть помещено в переменную sorted_words.
         """
+   
+sorted_words = []
+for i in two_dim_words:
+    for j in i:
+        sorted_words.append(j)
+sorted_words.sort()
+sorted_words.sort(key=len, reverse=True)
+
 
     return sorted_words
 
@@ -15,36 +23,118 @@ def task_3(numbers):
         Переменная numbers - ваша строка чисел.
         Финальное значение должно быть помещено в переменную dict_min.
         """
+    c = []
+    dict_min = {}
+    r = 0
+    v = 0
+    q = 0
+    a = 0
+    z = []
+    x = []
+    t = {}
+    w = 0
 
-    return dict_min
 
 
-def task_4_1(words):
+    for i in range(len(numbers)):
+        if numbers[i] in c:
+            continue
+        else:
+            x.append(numbers.count(numbers[i]))
+            c.append(numbers[i])
+    for s in c:
+     t[s] = x[w]
+        w += 1
+    
+    new_1 = t.copy()
+    p = max(new_1.values())
+    for i, j in new_1.items():
+        if j == p:
+            q = i
+            break
+    new_2 = t.copy()
+    new_2.pop(q)
+    d = max(new_2.values())
+    for i, j in new_2.items():
+        if j == d:
+            a = i
+            break
+    new_3 = new_2.copy()
+    new_3.pop(a)
+    k = max(new_3.values())
+    for i, j in new_3.items():
+        if j == k:
+            r = i
+            break
+    lst_1 = [q, a, p]
+    lst_2 = [p, d, r]
+    for o in lst_1:
+        dict_min[o] = lst_2[v]
+        v += 1
+    returndict_min
+
+
+def task_4_1(f):
+    """
+        Здесь должен быть ваш код.
+        Переменная words - ваш кортеж слов из задания.
+        Финальное значение должно быть помещено в переменную res
+        """
+
+    b=[]
+    c=set()
+    p=['e','u','o','a','i','y']
+    q=[]
+
+    for i in f:
+        m = i.lower()
+        if m.count('a')>=2:
+            b.append(m.count('a')**2)
+        else:
+            continue
+    res = tuple(b))
+    return res
+
+
+def task_4_2(f):  # можно сделать тесты
     """
         Здесь должен быть ваш код.
         Переменная words - ваш кортеж слов из задания.
         Финальное значение должно быть помещено в переменную res.
         """
+    
+    b=[]
+    c=set()
+    p=['e','u','o','a','i','y']
+    q=[]
+
+    for i in f:
+        if len(i)>3:
+            c.add(len(i))
+    res=c
 
     return res
 
 
-def task_4_2(words):  # можно сделать тесты
+def task_4_3(f):
     """
         Здесь должен быть ваш код.
         Переменная words - ваш кортеж слов из задания.
         Финальное значение должно быть помещено в переменную res.
         """
 
-    return res
+    b=[]
+    c=set()
+    p=['e','u','o','a','i','y']
+    q=[]
 
-
-def task_4_3(words):
-    """
-        Здесь должен быть ваш код.
-        Переменная words - ваш кортеж слов из задания.
-        Финальное значение должно быть помещено в переменную res.
-        """
+    for i in f:
+        l=i.lower()
+        if l.endswith('a'):
+            for j in l:
+                if j not in p:
+                    q.append(j)
+    res=q
 
     return res
 
@@ -56,15 +146,28 @@ def task_5(lst1, lst2):
         Финальное значение должно быть помещено в переменную diff.
         """
 
+    diff1 = []
+
+    d = lst2.intersection(lst1, lst2)
+    for i in lst1:
+        if i not in d:
+            diff1.append(i)
+    diff = sorted(diff1)
+
     return diff
 
 
-def task_6(lst):
+def task_6(c):
     """
         Здесь должен быть ваш код.
         Переменная lst - ваш список.
         Финальное значение должно быть помещено в переменную res.
         """
-
+    
+    c =[int(i) for i in input().split()]
+    d=[]
+    for i in c:
+        if c.count(i)==1:
+            d.append(i)
+    res = tuple(sorted(d, reverse=True))
     return res
-
